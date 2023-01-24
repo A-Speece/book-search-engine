369fae9e6b8870ac37c99aa3baefd405939e3957
Anthony Speece <anthonyspeece@gmail.com>
2023-01-23 23:42:12 -0600

    added apolloclient to App.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -4,6 +4,16 @@ export const ADD_USER = gql``;
 
 export const SAVE_BOOK = gql``;
 
-export const LOGIN_USER = gql``;
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
 
 export const REMOVE_BOOK = gql``;
